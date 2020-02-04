bf4c5604a8699bf499665234190259ef3f8fb833
anmolec <anmole_chaudry@hotmail.com>
2020-02-04 12:58:58 -0500

    Refactored echo command parsing

--- botimusprime/src/main/java/botimusprime/botimusprime/commands/EchoCommand.java
+++ botimusprime/src/main/java/botimusprime/botimusprime/commands/EchoCommand.java
@@ -1,22 +1,33 @@
 package botimusprime.botimusprime.commands;
 
+import java.util.Arrays;
+import java.util.List;
+
 import discord4j.core.event.domain.message.MessageCreateEvent;
 
 public class EchoCommand implements Command {
 	
 	@Override
 	public void execute(MessageCreateEvent event) {
-		System.out.println(event.getMessage().getContent());
-
-		String content = event.getMessage().getContent().toString();
-		String[] arguments = content.substring(content.indexOf('!'), content.length()-1).split(" ");
-		int repeats = Integer.parseInt(arguments[arguments.length-1]);
-		String username = arguments[1];
+		String content = event.getMessage().getContent().get();
+		List<String> args = Arrays.asList(content.split(" "));
+		
+		if (args.size() != 3)
+			return;
+		
+		String username = args.get(1);
+		int repeats = Integer.parseInt(args.get(2));
 		
 		for (int i = 0; i < repeats; i++) {			
 			event.getMessage()
 	        .getChannel().block()
 	        .createMessage(username).block();
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
