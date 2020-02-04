83104a337049d755e4657fa58cec2a79637dbd95
anmolec <anmole_chaudry@hotmail.com>
2020-02-04 12:44:48 -0500

    implemented pinging for remindme command

--- botimusprime/src/main/java/botimusprime/botimusprime/commands/RemindMeCommand.java
+++ botimusprime/src/main/java/botimusprime/botimusprime/commands/RemindMeCommand.java
@@ -6,8 +6,17 @@ public class RemindMeCommand implements Command {
 	
 	@Override
 	public void execute(MessageCreateEvent event) {
-		System.out.println(event.getMessage().getAuthor());
-		//TODO
+//		System.out.println(event.getMessage().getAuthor().get().getUsername());
+		long username = event.getMessage().getAuthor().get().getId().asLong();
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		event.getMessage()
+        .getChannel().block()
+        .createMessage("<@"+username+">").block();
 	}
 
 }
