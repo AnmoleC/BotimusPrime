32339e556f9e548ee899326880e536a9b0f19fab
anmolec <anmole_chaudry@hotmail.com>
2020-02-04 14:01:47 -0500

    implemented remindme command

--- botimusprime/src/main/java/botimusprime/botimusprime/commands/RemindMeCommand.java
+++ botimusprime/src/main/java/botimusprime/botimusprime/commands/RemindMeCommand.java
@@ -1,22 +1,50 @@
 package botimusprime.botimusprime.commands;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
 import discord4j.core.event.domain.message.MessageCreateEvent;
 
 public class RemindMeCommand implements Command {
+	private static final String parseErrMessage = "Invalid time format. Please specify a wait time as follows HH:mm";
 	
 	@Override
 	public void execute(MessageCreateEvent event) {
-//		System.out.println(event.getMessage().getAuthor().get().getUsername());
 		long username = event.getMessage().getAuthor().get().getId().asLong();
+		String content = event.getMessage().getContent().get();
+		List<String> args = Arrays.asList(content.split(" "));
+		
+		if (args.size() != 2)
+			return;
+		
+		DateFormat dateformat = new SimpleDateFormat("HH:mm");
 		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+			Date reference = dateformat.parse("00:00:00");
+			Date wake = dateformat.parse(args.get(1));
+			long miliseconds = wake.getTime() - reference.getTime();
+			System.out.println(miliseconds);
+			
+			Thread.sleep(miliseconds);
+
+			event.getMessage()
+	        .getChannel().block()
+	        .createMessage("<@"+username+">").block();
+		} catch (ParseException e1) {
+//			e1.printStackTrace();
+			
+			event.getMessage()
+	        .getChannel().block()
+	        .createMessage(parseErrMessage).block();
+		} catch (InterruptedException e2) {
+			e2.printStackTrace();
 		}
 		
-		event.getMessage()
-        .getChannel().block()
-        .createMessage("<@"+username+">").block();
+		
+		
 	}
 
 }
