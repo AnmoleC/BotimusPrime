5e9637f988bb29b16feee1a5b70640d436cac6cd
anmol <anmol@192.168.0.15>
2020-02-20 17:49:03 -0500

    Fixed Cookie warning when sending Danbooru Request

--- botimusprime/src/main/java/danbooru/DanbooruManager.java
+++ botimusprime/src/main/java/danbooru/DanbooruManager.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -17,9 +19,12 @@ public class DanbooruManager {
 	public static DanbooruResultBean fetchResults(String url){
 		HttpGet request = new HttpGet(url);
 		
-		try(CloseableHttpClient httpClient = HttpClients.createDefault();
-				CloseableHttpResponse response = httpClient.execute(request)){				
-				System.out.println(response.getStatusLine().toString());
+		try(CloseableHttpClient httpClient = HttpClients.custom()
+		        	.setDefaultRequestConfig(RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build())
+		        	.build();
+				CloseableHttpResponse response = httpClient.execute(request)){			
+				
+			System.out.println(response.getStatusLine().toString());
 				HttpEntity entity = response.getEntity();
 				if (entity != null){
 					String json = EntityUtils.toString(entity, StandardCharsets.UTF_8);
